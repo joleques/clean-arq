d20bc80d518ee5495f63364f2e80f95bd3f4b8d4
joleques <joleques@gmail.com>
2022-02-20 07:34:28 -0300

    Ajustando a descricao

--- node/test/domain/Pedido.test.ts
+++ node/test/domain/Pedido.test.ts
@@ -21,8 +21,8 @@ test("Deve fazer um pedido com cpf valido", () => {
 test("Deve fazer um pedido com 3 itens (com descrição, preço e quantidade)", () => {
     const pedido = new Pedido("355.203.280-05");
     pedido.addItem("123-ASGRTO-789", 1500, 2)
-    pedido.addItem("79845132-DSDUASYDKKN-879", 500, 1)
-    pedido.addItem("789-ARSGSHDJDY-456", 100, 1)
+    pedido.addItem("Guitarra", 500, 1)
+    pedido.addItem("Geladeira", 100, 1)
 
     expect(3600).toEqual(pedido.getTotal());
 })
@@ -35,8 +35,8 @@ test("Total do pedido deve ser zero quando não tem item", () => {
 test("Deve fazer um pedido com cupom de desconto (percentual sobre o total do pedido)", () => {
     const cupom = new CupomDesconto(10)
     const pedido = new Pedido("355.203.280-05");
-    pedido.addItem("852-GSTRUDJSHAST-741", 2000, 2)
-    pedido.addItem("79845132-DSDUASYDKKN-879", 1000, 1)
+    pedido.addItem("Camera", 2000, 2)
+    pedido.addItem("Guitarra", 1000, 1)
     pedido.addCupomDesconto(cupom)
 
     expect(4500).toEqual(pedido.getTotal());
@@ -45,8 +45,8 @@ test("Deve fazer um pedido com cupom de desconto (percentual sobre o total do pe
 test("Não deve aplicar cupom de desconto expirado", () => {
     const cupom = new CupomDesconto(10, new Date(Date.now() - 100000))
     const pedido = new Pedido("355.203.280-05");
-    pedido.addItem("852-GSTRUDJSHAST-741", 2000, 2)
-    pedido.addItem("79845132-DSDUASYDKKN-879", 1000, 1)
+    pedido.addItem("Camera", 2000, 2)
+    pedido.addItem("Guitarra", 1000, 1)
     pedido.addCupomDesconto(cupom)
 
     expect(5000).toEqual(pedido.getTotal());
@@ -56,8 +56,8 @@ test("Não deve aplicar cupom de desconto expirado", () => {
 test("Deve aplicar cupom de desconto quando a data de expiração é maior que a data atual", () => {
     const cupom = new CupomDesconto(10, new Date(Date.now() + 100000))
     const pedido = new Pedido("355.203.280-05");
-    pedido.addItem("852-GSTRUDJSHAST-741", 2000, 2)
-    pedido.addItem("79845132-DSDUASYDKKN-879", 1000, 1)
+    pedido.addItem("Camera", 2000, 2)
+    pedido.addItem("Guitarra", 1000, 1)
     pedido.addCupomDesconto(cupom)
 
     expect(4500).toEqual(pedido.getTotal());
